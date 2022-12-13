c07f382c894f38a53fa9cfcefaee857d0f26d9ec
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-13 17:09:42 +0900

    style(InputView): 투입 금액 입력 및 유효성 검사 기능 추가
    docs: 투입 금액 입력, `InputValidation` → 돈 금액 입력 유효성 검사 완료 체크

--- src/main/java/vendingmachine/InputView.java
+++ src/main/java/vendingmachine/InputView.java
@@ -8,6 +8,7 @@ import java.util.Map;
 public class InputView {
     private static final String INPUT_VENDING_MACHINE_AMOUNT = "자판기가 보유하고 있는 금액을 입력해 주세요.";
     private static final String INPUT_VENDING_MACHINE_PRODUCTS = "상품명과 가격, 수량을 입력해 주세요.";
+    private static final String INPUT_CONSUMER_BUDGET = "투입 금액을 입력해 주세요.";
     private static final String SPACE = " ";
     private static final String NONE = "";
 
@@ -21,5 +22,10 @@ public class InputView {
     public List<Map<String, Object>> readVendingMachineProducts() {
         System.out.println(INPUT_VENDING_MACHINE_PRODUCTS);
         return inputValidation.validateProducts(Console.readLine().replaceAll(SPACE, NONE));
+    }
+
+    public int readConsumerBudget() {
+        System.out.println(INPUT_CONSUMER_BUDGET);
+        return inputValidation.validateAmount(Console.readLine());
     }
 }
