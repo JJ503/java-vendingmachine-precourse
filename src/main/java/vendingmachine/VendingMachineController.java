6afb0ba7961fbb4812997c8a0cb93cff8acdefe9
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-13 16:44:35 +0900

    feat(VendingMachineController): 상품 정보 입력 기능 추가
    docs: 상품 정보 입력 완료 체크

--- src/main/java/vendingmachine/VendingMachineController.java
+++ src/main/java/vendingmachine/VendingMachineController.java
@@ -1,5 +1,8 @@
 package vendingmachine;
 
+import java.util.List;
+import java.util.Map;
+
 public class VendingMachineController {
     InputView inputView = new InputView();
     OutputView outputView = new OutputView();
@@ -7,9 +10,14 @@ public class VendingMachineController {
     public void operateVendingMachine() {
         VendingMachine vendingMachine = new VendingMachine(enterVendingMachineAmount());
         outputView.printVendingMachineCoins(vendingMachine.getVendingMachineCoins());
+        Products products = new Products();
     }
 
     private int enterVendingMachineAmount() {
         return inputView.readVendingMachineAmount();
+    }
+
+    private List<Map<String, Object>> enterVendingMachineProducts() {
+        return inputView.readVendingMachineProducts();
     }
 }
