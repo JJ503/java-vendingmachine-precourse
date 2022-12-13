18db18a3ceee94d260db18a2e9e068b8fa117be1
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-13 15:41:38 +0900

    feat(VendingMachine): 자판기 보유 동전 출력 기능 추가
    docs: 자판기 보유 동전 출력 완료 체크

--- src/main/java/vendingmachine/VendingMachineController.java
+++ src/main/java/vendingmachine/VendingMachineController.java
@@ -6,6 +6,7 @@ public class VendingMachineController {
 
     public void operateVendingMachine() {
         VendingMachine vendingMachine = new VendingMachine(enterVendingMachineAmount());
+        outputView.printVendingMachineCoins(vendingMachine.getVendingMachineCoins());
     }
 
     private int enterVendingMachineAmount() {
