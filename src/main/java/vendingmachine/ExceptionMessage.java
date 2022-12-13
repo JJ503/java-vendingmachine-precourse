04fcfd3584105d373e5889d84988c49953b51b77
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-13 16:43:15 +0900

    feat(ExceptionMessage): 예외 메시지 상수 추가
    - 상품 수량이 1미만인 경우

--- src/main/java/vendingmachine/ExceptionMessage.java
+++ src/main/java/vendingmachine/ExceptionMessage.java
@@ -4,7 +4,8 @@ public enum ExceptionMessage {
     ENTER_NOTHING("값을 입력해야 합니다."),
     IS_NOT_NUMBER("금액은 숫자여야 합니다."),
     IS_NOT_DIVIDED_BY_10("금액은 10으로 나누어 떨어져야 합니다."),
-    IS_NOT_THREE_INFORMATION("상품 정보는 상품영, 가격, 수량을 입력해야 합니다.");
+    IS_NOT_THREE_INFORMATION("상품 정보는 상품영, 가격, 수량을 입력해야 합니다."),
+    IS_NOT_OVER_ZERO("상품 수량은 1 이상이어야 합니다.");
 
     private static final String ERROR_TAG = "[ERROR] ";
     private final String errorMessage;
