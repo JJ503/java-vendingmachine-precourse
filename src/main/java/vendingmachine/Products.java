151fa8961c00de8760eb71985aadd559a831730e
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-13 16:45:37 +0900

    feat(Products): 상품 정보 추가 기능 추가
    docs: 상품 정보 추가 완료 체크

--- src/main/java/vendingmachine/Products.java
+++ src/main/java/vendingmachine/Products.java
@@ -1,4 +1,12 @@
 package vendingmachine;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Products {
+    private final List<Product> products = new ArrayList<>();
+
+    public void addProduct(Product newProduct) {
+        products.add(newProduct);
+    }
 }
