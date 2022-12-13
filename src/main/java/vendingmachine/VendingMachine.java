232b677a3ef16ab195f221f253ff5ac15bdbdac2
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-13 15:18:57 +0900

    feat(VendingMachine): 보유 금액 입력에 따른 보유 동전 개수 저장 기능 추가
    docs: 보유 금액 입력에 따른 보유 동전 개수 저장, 금액 내에서 동전을 랜덤으로 생성 완료 체크

--- src/main/java/vendingmachine/VendingMachine.java
+++ src/main/java/vendingmachine/VendingMachine.java
@@ -8,6 +8,7 @@ public class VendingMachine {
 
     VendingMachine(int amount) {
         vendingMachineCoins = initVendingMachineCoins();
+        amountToCoinList(amount);
     }
 
     private HashMap initVendingMachineCoins() {
@@ -17,5 +18,22 @@ public class VendingMachine {
             put(Coin.COIN_100, 0);
             put(Coin.COIN_500, 0);
         }};
+    }
+
+    private void amountToCoinList(int amount) {
+        while (amount > 0) {
+            amount = createRandomCoin(amount);
+        }
+    }
+
+    private int createRandomCoin(int amount) {
+        Coin randomCoin = Coin.getCoin(RandomCoinAmount.randomCoinAmount());
+        int subtractValue = randomCoin.subtractValue(amount);
+        if (subtractValue > 0) {
+            vendingMachineCoins.put(randomCoin, vendingMachineCoins.get(randomCoin) + 1);
+            return subtractValue;
+        }
+
+        return amount;
     }
 }
