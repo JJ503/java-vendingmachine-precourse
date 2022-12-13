8a6191e41a85637495bae71207c386c43e41cd6e
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-13 17:06:40 +0900

    fix(InputView): 입력 값에 띄어쓰기가 있는 경우에 대한 문제 해결

--- src/main/java/vendingmachine/InputView.java
+++ src/main/java/vendingmachine/InputView.java
@@ -8,6 +8,8 @@ import java.util.Map;
 public class InputView {
     private static final String INPUT_VENDING_MACHINE_AMOUNT = "자판기가 보유하고 있는 금액을 입력해 주세요.";
     private static final String INPUT_VENDING_MACHINE_PRODUCTS = "상품명과 가격, 수량을 입력해 주세요.";
+    private static final String SPACE = " ";
+    private static final String NONE = "";
 
     private final InputValidation inputValidation = new InputValidation();
 
@@ -18,6 +20,6 @@ public class InputView {
 
     public List<Map<String, Object>> readVendingMachineProducts() {
         System.out.println(INPUT_VENDING_MACHINE_PRODUCTS);
-        return inputValidation.validateProducts(Console.readLine());
+        return inputValidation.validateProducts(Console.readLine().replaceAll(SPACE, NONE));
     }
 }
