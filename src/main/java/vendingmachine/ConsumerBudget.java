c67db3ef94c364895ba8aa59b27f5823a15d840f
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-13 17:42:20 +0900

    feat(Product): 현재 투입 금액 반환하기 기능 추가
    docs: 현재 투입 금액 반환하기 완료 체크

--- src/main/java/vendingmachine/ConsumerBudget.java
+++ src/main/java/vendingmachine/ConsumerBudget.java
@@ -1,9 +1,15 @@
 package vendingmachine;
 
+import java.util.Collections;
+
 public class ConsumerBudget {
     private final int budget;
 
     ConsumerBudget(int budget) {
         this.budget = budget;
+    }
+
+    public int getBudget() {
+        return budget;
     }
 }
