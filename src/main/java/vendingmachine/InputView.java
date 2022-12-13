744b3a71d28cc139fd168f928ee9cedeeb89e370
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-13 14:04:54 +0900

    feat(InputView): 돈 급액 입력과 유효성 검사 연결
    docs: `InputValidation` → 돈 금액 입력 유효성 검사 완료 체크

--- src/main/java/vendingmachine/InputView.java
+++ src/main/java/vendingmachine/InputView.java
@@ -4,9 +4,10 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String INPUT_VENDING_MACHINE_AMOUNT = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+    private final InputValidation inputValidation = new InputValidation();
 
-    public String readVendingMachineAmount() {
+    public int readVendingMachineAmount() {
         System.out.println(INPUT_VENDING_MACHINE_AMOUNT);
-        return Console.readLine();
+        return inputValidation.validateAmount(Console.readLine());
     }
 }
