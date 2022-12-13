bed2d492a373d20d4aa47538fdce3afa0f9db424
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-13 17:13:40 +0900

    feat(OutputView): 투입 금액 현황 출력 기능 추가
    docs: 투입 금액 현황 출력 완료 체크

--- src/main/java/vendingmachine/OutputView.java
+++ src/main/java/vendingmachine/OutputView.java
@@ -2,9 +2,14 @@ package vendingmachine;
 
 public class OutputView {
     private static final String PRINT_VENDING_MACHINE_COINS = "\n자판기가 보유한 동전";
+    private static final String PRINT_CONSUMER_BUDGET = "\n투입 금액: %d원\n";
 
     public void printVendingMachineCoins(String vendingMachineCoins) {
         System.out.println(PRINT_VENDING_MACHINE_COINS);
         System.out.println(vendingMachineCoins);
+    }
+
+    public void printConsumerBudget(int budget) {
+        System.out.printf(PRINT_CONSUMER_BUDGET, budget);
     }
 }
