9c4b1a0f993600cdb28932e4070c74327025bde4
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-12-13 15:59:29 +0900

    feat(InputValidation): 상품 정보 입력 유휴성 검사 기능 추가
    docs: 상품 정보 입력 유휴성 검사 및 관련 기능 완료 체크

--- src/main/java/vendingmachine/InputValidation.java
+++ src/main/java/vendingmachine/InputValidation.java
@@ -1,7 +1,13 @@
 package vendingmachine;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class InputValidation {
     private static final int UNIT_OF_MONEY = 10;
+    private static final String SEMICOLON = ";";
+    private static final String COMMA = ",";
 
     public int validateAmount(String checkAmount) {
         isExistValue(checkAmount);
@@ -9,6 +15,11 @@ public class InputValidation {
         isDividedBy10(amount);
 
         return amount;
+    }
+
+    public List<List<String>> validateProducts(String checkProducts) {
+        isExistValue(checkProducts);
+        return getProductsInformationList(checkProducts);
     }
 
     private void isExistValue(String checkValue) {
@@ -31,5 +42,26 @@ public class InputValidation {
         if (checkValue % UNIT_OF_MONEY != 0) {
             ExceptionMessage.IS_NOT_DIVIDED_BY_10.throwException();
         }
+    }
+
+    private List<List<String>> getProductsInformationList(String checkValue) {
+        List<String> products = Arrays.asList(checkValue.split(SEMICOLON));
+        List<List<String>> productsInforms = new ArrayList<>();
+
+        for (String product : products) {
+            productsInforms.add(isThreeInformation(product));
+        }
+
+        return productsInforms;
+    }
+
+    private List<String> isThreeInformation(String product) {
+        List<String> productInforms = Arrays.asList(product.split(COMMA));
+        if (productInforms.size() == 3) {
+            return productInforms;
+        }
+
+        ExceptionMessage.IS_NOT_THREE_INFORMATION.throwException();
+        return null;
     }
 }
